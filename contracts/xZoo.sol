7f97f185bcaf56173c0da0dbad77dee5cbe66fc8
Dmitry Korovin <korovin.ds@aurrency.com>
2023-01-18 05:03:12 +0300

    Updated licenses

--- contracts/xZoo.sol
+++ contracts/xZoo.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.8.13;
 
+// SPDX-License-Identifier: MIT
 
 import "OpenZeppelin/openzeppelin-contracts@4.5.0/contracts/token/ERC20/IERC20.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.5.0/contracts/token/ERC721/ERC721.sol";
